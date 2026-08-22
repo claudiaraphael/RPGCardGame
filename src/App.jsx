2e4280e0a43413fb7bf1fa48629bd0adf7e7df28
claudia <50813092+claudiaraphael@users.noreply.github.com>
2026-08-22 01:07:20 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import Header from './components/Header.jsx'
+import Footer from './components/Footer.jsx'
+import Food from './components/Food.tsx'
+import Card from './components/Card.tsx'
+import Button from './components/Button.jsx'
+
+function App() {
+  return(
+    <>
+      <Header/>
+      <Food/>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Button/>
+      <Footer/>
+    
+    </>
+        
+  );
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+
+function Header() {
+    return (
+        <header>
+            <h1>RPG Card Game</h1>
+            <nav>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Bestiary</a></li>
+                    <li><a href="#">Characters</a></li>
+                    <li><a href="#">Cards</a></li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default Header
